7ff426199c29eba711a4bbab65f6bad21b25fa56
wzyexf <wzyexf@126.com>
2017-01-03 14:34:41 +0800

    初始化

---
+++ design/design/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(design.Startup))]
+namespace design
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
